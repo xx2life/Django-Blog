0976bffcd239152a7da942680bae3aa94eeb7e47
xx2life <xx2life@outlook.com>
2020-09-25 15:44:53 +0800

    配置时区

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xu6oevfo2comm$=df(g3fc^g*bb^xzgl9g%w=-fyxv$c%jl_0u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
